29cdb4053c49b81bd10e515e027d29f96ca30c8d
davidayobami0311-ux <davidayobami0311@gmail.com>
2025-10-08 17:19:09 +0100

    Update settings.js for TANGIRO

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~90c8185723a421cc09c315a18196a22ed07c04ac",
+  OWNER_NUMBER: "+2348081350084", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
